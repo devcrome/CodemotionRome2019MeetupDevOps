62b9a3df2acc77b1f7bca7a13fe63f0385eeecec
dm.lagosmorales <dm.lagosmorales@almaviva.it>
2019-03-22 14:32:19 +0100

    try to load git commit from github

--- kube_deploy.groovy
+++ kube_deploy.groovy
@@ -5,18 +5,13 @@ pipeline {
             steps {
                 script {
                     echo sh(script: 'env|sort', returnStdout: true)
-                    GIT_COMMIT_SHORT = sh(
-                        script: "printf \$(git rev-parse --short ${GIT_COMMIT})",
-                        returnStdout: true
-                    )
-                    echo "${GIT_COMMIT_SHORT}"
-
                     gitProjectUrl = "${pipe_project_url}"
                     gitProjectBranch = "${pipe_project_branch}"
                     namespace = "${pipe_project_branch}"
 
                     slaveAbsolutePath = pwd()
                     kubeConfigPath = "${slaveAbsolutePath}"
+                    k8sFolder = 'k8s'
 
                     currentBuild.displayName = "${env.BUILD_DISPLAY_NAME}-${GIT_COMMIT_SHORT}"
                 }
@@ -30,7 +25,7 @@ pipeline {
                               branches                         : [[name: "${gitProjectBranch}"]],
                               doGenerateSubmoduleConfigurations: false,
                               extensions                       : [[$class           : 'RelativeTargetDirectory',
-                                                                   relativeTargetDir: "."],
+                                                                   relativeTargetDir: "${k8sFolder}"],
                                                                   [$class: 'CloneOption', shallow: true]],
                               submoduleCfg                     : [],
                               userRemoteConfigs                : [[
@@ -42,24 +37,44 @@ pipeline {
                 }
             }
         }
+        stage('Load git infos') {
+            dir("${k8sFolder}") {
+                steps {
+                    script {
+                        GIT_COMMIT_SHORT = sh(
+                            script: "printf \$(git rev-parse --short ${GIT_COMMIT})",
+                            returnStdout: true
+                        )
+                        echo "${GIT_COMMIT_SHORT}"
+                        currentBuild.displayName = "${env.BUILD_DISPLAY_NAME}-${GIT_COMMIT_SHORT}"
+                    }
+                }
+            }
+        }
         stage('Deploy on kubernetes') {
-            steps {
-                script {
-                    sh "kubectl apply -f . "
+            dir("${k8sFolder}") {
+                steps {
+                    script {
+                        sh "kubectl apply -f . "
+                    }
                 }
             }
         }
         stage('Rollout status') {
-            steps {
-                script {
-                    sh "kubectl rollout status deployment/codemotion-dev-circle-front --namespace ${namespace}"
+            dir("${k8sFolder}") {
+                steps {
+                    script {
+                        sh "kubectl rollout status deployment/codemotion-dev-circle-front --namespace ${namespace}"
+                    }
                 }
             }
         }
         stage('Watch service status') {
-            steps {
-                script {
-                    sh "kubectl get service codemotion-dev-circle-front --namespace ${namespace}"
+            dir("${k8sFolder}") {
+                steps {
+                    script {
+                        sh "kubectl get service codemotion-dev-circle-front --namespace ${namespace}"
+                    }
                 }
             }
         }
